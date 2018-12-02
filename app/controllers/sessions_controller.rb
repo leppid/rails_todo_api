0be690b1d7368774bbd142bd2f4155bd677a2dc3
leppid <myvoden@gmail.com>
2018-12-02 17:36:34 +0200

    add notifications

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,13 +8,13 @@ class SessionsController < ApplicationController
        if user.activated
         render json: { user: user }, status: 200
        else
-          render json: { message: 'Account not activated. Check yor email for the activation link' }, status: 207
+          render json: { message: 'Account not activated. Check yor email for the activation link', status: 'info' }, status: 207
        end
       else
-        render json: { message: 'Invalid Email or Password' }, status: 207
+        render json: { message: 'Invalid Email or Password', status: 'error' }, status: 207
       end
     else
-      render json: { message: 'User doesnt exist' }, status: 207
+      render json: { message: 'User doesnt exist', status: 'error' }, status: 207
     end
 
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,29 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save!
-      origin = request.headers['origin']
-      UserMailer.account_activation(@user, origin).deliver
-      render json: { message: 'User Saved!' }, status: 200
+    if @user.password === @user.password_confirmation
+      email_exists = User.find_by(email: @user.email)
+      if !email_exists
+        origin = request.headers['origin']
+        UserMailer.account_activation(@user, origin).deliver
+        if @user.save!
+          render json: { message: 'Almost done! Please check your email for activate your account.', status: 'success' }, status: 200
+        else
+          render json: { message: User.create(user_params).errors, status: 'error'}, status: 404 
+        end
+      else
+        render json: { message: 'Email Exists!', status: 'error' }, status: 207 
+      end
+    else
+      render json: { message: 'Passwords do not match', status: 'error' }, status: 207
     end
   end
+
 
   def confirmemail
     user = User.find_by_activation_digest(params[:user])
     if user && user.update_column(:activated, true)
-      render json: { message: 'Email Confirmed!' }, status: 200
+      render json: { message: 'Account successfly activated!', status: 'success' }, status: 200
     end
   end
 
