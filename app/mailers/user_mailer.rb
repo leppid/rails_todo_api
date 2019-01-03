b8c0f7edff6a47d7a2641331ed9803f437f2478a
leppid <myvoden@gmail.com>
2019-01-03 17:06:43 +0200

    deploying

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def account_activation(user, origin)
     @user = user
     @origin = origin
-    mail(to: user.email, Subject: 'TaskApp: Account Activation')
+    mail(to: @user.email, Subject: 'TaskApp: Account Activation')
   end
 end
