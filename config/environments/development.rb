b41596ecaab3ca9610b9a74c63807d302d556595
leppid <myvoden@gmail.com>
2019-01-03 17:34:28 +0200

    deploying

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.default_url_options = { :host => "dtodo.herokuapp.com" }
 
   config.action_mailer.delivery_method = :smtp
