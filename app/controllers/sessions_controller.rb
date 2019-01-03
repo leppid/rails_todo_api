e2d1e9618debf1a3a2cbd34311258c9fc21ea5f0
leppid <myvoden@gmail.com>
2019-01-03 16:50:45 +0200

    deploying

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user
       if user.authenticate(params[:session][:password])
-        if user.activate
+        if user.activated
           render json: { user: user }, status: 200
         else
           render json: { message: 'Account not activated. Check yor email for the activation link', status: 'info' }, status: 404
--- db/migrate/20181126120250_create_users.rb
+++ db/migrate/20181126120250_create_users.rb
@@ -5,7 +5,6 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string :firstname
       t.string :lastname
       t.string :password_digest
-      t.string :activation_digest
 
       t.timestamps
     end
