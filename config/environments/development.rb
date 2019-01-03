b02028aba25b1fbecd4d35238026b28aa204ec46
leppid <myvoden@gmail.com>
2019-01-03 17:15:14 +0200

    deploying

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
-    port: 587,
+    port: 465,
     domain: 'smtp.gmail.com',
     user_name: 'aacaut2@gmail.com',
     password: 'jVS9mCrJ',
