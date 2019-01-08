59601a1b3b5d2ab0e0ce56d42b02c78b78ab1d2b
leppid <myvoden@gmail.com>
2019-01-08 11:17:23 +0200

    deploying

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,21 +51,23 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "api_app_name_#{Rails.env}"
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { :host => "https://dtodo.herokuapp.com/" }
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'https://dtodo.herokuapp.com/' }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
+
   config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :domain => 'dtodo.herokuapp.com',
-      :user_name => "aacaut2@gmail.com",
-      :password => "jVS9mCrJ",
-      :authentication => :plain,
-      :enable_starttls_auto => true
-  }
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'smtp.gmail.com',
+  user_name: 'aacaut2@gmail.com',
+  password: 'jVS9mCrJ',
+  enable_starttls_auto: true ,
+  authentication: :plain
+}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
