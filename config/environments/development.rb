9266ffa2ac9ce2a23687e416ed93d576c0b12d62
leppid <myvoden@gmail.com>
2019-01-08 11:00:35 +0200

    deploying

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,22 +27,20 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.perform_caching = false
-
-  config.action_mailer.default_url_options = { :host => "https://dtodo.herokuapp.com/" }
-
   config.action_mailer.delivery_method = :smtp
-
+  config.action_mailer.default_url_options = { host:'https://dtodo.herokuapp.com/'}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-  address: 'smtp.gmail.com',
-  port: 587,
-  user_name: 'aacaut2@gmail.com',
-  password: 'jVS9mCrJ',
-  enable_starttls_auto: true ,
-  authentication: :plain
-}
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => 'dtodo.herokuapp.com',
+      :user_name => "aacaut2@gmail.com",
+      :password => "jVS9mCrJ",
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
